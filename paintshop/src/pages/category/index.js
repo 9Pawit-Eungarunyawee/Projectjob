f91f2114f8354a9f12ab6632e21283309c81d25c
Pawit Eungarunyawee <Pawit.e@kkumail.com>
2023-07-15 13:52:56 +0700

    sidebar

--- paintshop/src/pages/category/index.js
+++ paintshop/src/pages/category/index.js
@@ -3,12 +3,12 @@ import Box from "@mui/material/Box";
 import Layout from "@/components/layout";
 import { Typography } from "@mui/material";
 
-export default function Dashboard() {
+export default function Category() {
   return (
     <>
       <Layout>
         <Box sx={{ height: "100vh", width: "100%" }}>
-          <Typography>sada</Typography>
+          <Typography>category</Typography>
         </Box>
       </Layout>
     </>
--- paintshop/src/pages/index.js
+++ paintshop/src/pages/index.js
@@ -1,11 +1,22 @@
 import Link from "next/link";
+import * as React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
 
 export default function App() {
-    return (
-        <>
-        <h1>Home</h1>
-        <Link href="/dashboard">Dashboard</Link>
-        <Link href="/product">Procut</Link>
-        </>
-    );
+  return (
+   
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="sx" sx={{padding:0}}>
+        <Box>
+          <h1>Home</h1>
+          <Link href="/dashboard">Dashboard</Link>
+          <Link href="/product">Procut</Link>
+        </Box>
+      </Container>
+    </React.Fragment>
+    
+  );
 }

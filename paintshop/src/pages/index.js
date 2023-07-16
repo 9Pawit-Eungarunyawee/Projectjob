387621137d8972fad707e3a1973bf9e5f7eedadb
carouse1123 <Theeraphong.s@kkumail.com>
2023-07-16 08:55:54 +0700

    navbar

--- paintshop/src/pages/index.js
+++ paintshop/src/pages/index.js
@@ -3,20 +3,15 @@ import * as React from "react";
 import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
-
+import Homelayout from "@/components/homelayout";
 export default function App() {
   return (
-   
-    <React.Fragment>
-      <CssBaseline />
-      <Container maxWidth="sx" sx={{padding:0}}>
-        <Box>
-          <h1>Home</h1>
-          <Link href="/dashboard">Dashboard</Link>
-          <Link href="/product">Procut</Link>
-        </Box>
-      </Container>
-    </React.Fragment>
-    
+    <Homelayout>
+      <Box>
+        <h1>Home</h1>
+        <Link href="/dashboard">Dashboard</Link>
+        <Link href="/product">Procut</Link>
+      </Box>
+    </Homelayout>
   );
 }

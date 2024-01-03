b4e18dc79ddea5c06ddc17d5f028363a1ed05f63
carouse11 <theeraphong.s@kkumail.com>
2024-01-04 01:36:14 +0700

    navbarfix

--- paintshop/src/firebase/getData.js
+++ paintshop/src/firebase/getData.js
@@ -1,5 +1,5 @@
 import firebase_app from "../firebase/config";
-import { getFirestore, doc, getDoc ,where,query} from "firebase/firestore";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
 
 const db = getFirestore(firebase_app);
 

cd388130a2e25fb56b1c552946bb5593dbb51dc5
carouse11 <theeraphong.s@kkumail.com>
2024-01-29 12:50:54 +0700

    orderandcart

--- paintshop/src/firebase/addOrder.js
+++ paintshop/src/firebase/addOrder.js
@@ -5,6 +5,7 @@ import {
   collection,
   addDoc,
   setDoc,
+  deleteDoc,
 } from "firebase/firestore";
 
 const db = getFirestore(firebase_app);
@@ -12,19 +13,19 @@ const db = getFirestore(firebase_app);
 export default async function addOrder(collectionName, data) {
   let result = null;
   let error = null;
-console.log("wwwwww",data)
+  console.log("wwwwww", data);
   // สร้างอ้างอิงไปยังเอกสารในคอลเลคชัน "cart"
   const currentDate = new Date();
   const img = data.img ? data.img : "";
   try {
     // เตรียมข้อมูลที่ต้องการเพิ่ม
     const orderData = {
-      cart_id: data.cart_id, // ใช้อ้างอิง
-      user_id:data.user_id,
+      product_id: data.product_id, // ใช้อ้างอิง
+      user_id: data.user_id,
       price: data.price,
-      date:currentDate,
+      date: currentDate,
       img: img,
-      status:data.status,
+      status: data.status,
     };
     console.log(orderData);
     // เพิ่มข้อมูลเข้าคอลเลคชันตาม collectionName ด้วยข้อมูลที่เตรียมไว้
@@ -38,3 +39,28 @@ console.log("wwwwww",data)
 
   return { result, error };
 }
+const deleteCartItem = async (cart_ids) => {
+  let result = null;
+  let error = null;
+
+  try {
+    // Loop through each cart_id in the array and delete the document
+    await Promise.all(
+      cart_ids.map(async (cart_id) => {
+        const cartDocRef = doc(db, "cart", cart_id);
+        await deleteDoc(cartDocRef);
+      })
+    );
+
+    console.log("ลบข้อมูลสำเร็จ");
+    result = true;
+  } catch (e) {
+    error = e;
+    console.error("เกิดข้อผิดพลาดในการลบข้อมูล:", error);
+    result = false;
+  }
+
+  return { result, error };
+};
+
+export { deleteCartItem };

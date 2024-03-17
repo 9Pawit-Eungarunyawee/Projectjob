568f8c8057df13baa893b29ade9f6e25ee5ca6eb
carouse11 <theeraphong.s@kkumail.com>
2024-03-17 10:46:51 +0700

    bugfixreq8

---
+++ paintshop/src/pages/cart/calculateshiprate.js
@@ -0,0 +1,21 @@
+// calculateShippingRate.js
+
+// สร้างฟังก์ชัน calculateShippingRate และทำการ export ออกมา
+export function calculateShippingRate(weight) {
+    let shipRate = 0;
+  
+    if (weight <= 1) {
+      shipRate = 20;
+    } else if (weight <= 4) {
+      shipRate = 40;
+    } else if (weight <= 10) {
+      shipRate = 100;
+    } else if (weight <= 19) {
+      shipRate = 190;
+    } else {
+      shipRate = 200;
+    }
+  
+    return shipRate;
+  }
+  
